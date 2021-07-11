30e9247856cd72b15eaf04868234a9eb32eb9caa
david <deividasmataitis@gmail.com>
2021-07-11 12:24:59 +0100

    added to display

--- app.dev.js
+++ app.dev.js
@@ -1,101 +1,37 @@
 "use strict";
 
-var resultScreen = document.getElementsByClassName('div__result');
-var operators = document.querySelectorAll('.btn__orange.innerHTML');
+//Consts
+var resultScreen = document.querySelector('.p');
+var operators = document.querySelectorAll('.btn__orange');
+console.log(operators);
 var clear = document.getElementById('clear');
-var button0 = document.getElementsByClassName('0')[0];
-var button1 = document.getElementsByClassName('1')[0];
-var button2 = document.getElementsByClassName('2')[0];
-var button3 = document.getElementsByClassName('3')[0];
-var button4 = document.getElementsByClassName('4')[0];
-var button5 = document.getElementsByClassName('5')[0];
-var button6 = document.getElementsByClassName('6')[0];
-var button7 = document.getElementsByClassName('7')[0];
-var button8 = document.getElementsByClassName('8')[0];
-var button9 = document.getElementsByClassName('9')[0];
-clear.addEventListener('click', function (e) {
-  var newResult = [" "];
-  resultScreen.innerHTML = newResult;
-});
-operators.forEach(function (operator) {
-  operator.addEventListener('click', function (e) {
-    resultScreen.innerHTML += e.target.innerHTML;
+var numb = document.querySelectorAll('.num');
+console.log(numb); //Variables
+
+Display = []; //Number button
+
+var _loop = function _loop(i) {
+  numb[i].addEventListener("click", function () {
+    Display.push(numb[i].innerHTML);
+    resultScreen.innerHTML = Display.join('');
+    console.log(resultScreen.innerHTML);
   });
-});
-button0.addEventListener('click', function (e) {
-  button0.classList.add('new');
-  var newResult = [button0.innerHTML];
-  var resultScreen = document.querySelector('p');
-  resultScreen.innerHTML += newResult;
-});
-button1.addEventListener('click', function (e) {
-  button1.classList.add('new');
-  var newResult = [button1.innerHTML];
-  var resultScreen = document.querySelector('p');
-  resultScreen.innerHTML += newResult;
-});
-button2.addEventListener('click', function (e) {
-  button2.classList.add('new');
-  var newResult = [button2.innerHTML];
-  var resultScreen = document.querySelector('p');
-  resultScreen.innerHTML += newResult;
-});
-button3.addEventListener('click', function (e) {
-  button3.classList.add('new');
-  var newResult = [button3.innerHTML];
-  var resultScreen = document.querySelector('p');
-  resultScreen.innerHTML += newResult;
-});
-button4.addEventListener('click', function (e) {
-  button4.classList.add('new');
-  var newResult = [button4.innerHTML];
-  var resultScreen = document.querySelector('p');
-  resultScreen.innerHTML += newResult;
-});
-button5.addEventListener('click', function (e) {
-  button5.classList.add('new');
-  var newResult = [button5.innerHTML];
-  var resultScreen = document.querySelector('p');
-  resultScreen.innerHTML += newResult;
-});
-button6.addEventListener('click', function (e) {
-  button6.classList.add('new');
-  var newResult = [button6.innerHTML];
-  var resultScreen = document.querySelector('p');
-  resultScreen.innerHTML += newResult;
-});
-button7.addEventListener('click', function (e) {
-  button7.classList.add('new');
-  var newResult = [button7.innerHTML];
-  var resultScreen = document.querySelector('p');
-  resultScreen.innerHTML += newResult;
-});
-button8.addEventListener('click', function (e) {
-  button8.classList.add('new');
-  var newResult = [button8.innerHTML];
-  var resultScreen = document.querySelector('p');
-  resultScreen.innerHTML += newResult;
-});
-button9.addEventListener('click', function (e) {
-  button9.classList.add('new');
-  var newResult = [button9.innerHTML];
-  var resultScreen = document.querySelector('p');
-  resultScreen.innerHTML += newResult;
-}); //buttonFunction.addEventListener('click',(e)=>{
-//let newResult =[buttonFunction.innerHTML];
-//const resultScreen=document.querySelector('p');
-//resultScreen.innerHTML=newResult;
-//})
-//create new variable as array to store resultScreen data           
-//when (=) is pressed split array (commas) and convert to mathematical equation 
-// For example [1,+,5,-,4] -> 1+5-4 -> assign to variable which when display result of equation on the screen
-//resultScreen=addResult.split(',') or similar ->extract array to make it plain which is basically math function
-//add the answer of math function to result screen
-//split array -> make it as mathematical function -> display result on = being pressed
-//additional-> c to clear the screen (seperate function)
-//if(resultSceen != 0){resultScreen=0} or similar
-// +/- to make positive and negative numbers
-// convert either result or button to +/- i.e. if result is 5 it can become -5 after pressing +/- button
-// [5,+,9]->[14]-> press +/- becomes [-,14] or [5] -> +/- -> [-,5] -> +4 ->[-,5,+,4] -> [-1]
-// % makes number n/100 
-//takes result or current number in array and divides from 100
+};
+
+for (var i = 0; i < numb.length; i++) {
+  _loop(i);
+} //Ops button
+
+
+var _loop2 = function _loop2(_i) {
+  operators[_i].addEventListener("click", function () {
+    Display.push(operators[_i].innerHTML);
+    var news = Display.join('');
+    resultScreen.innerHTML = news;
+    console.log(resultScreen.innerHTML);
+  });
+};
+
+for (var _i = 0; _i < operators.length; _i++) {
+  _loop2(_i);
+}
--- app.js
+++ app.js
@@ -1,150 +1,40 @@
 
-const resultScreen=document.getElementsByClassName('div__result');
 
-const operators=document.querySelectorAll('.btn__orange.innerHTML');
+//Consts
+
+const resultScreen=document.querySelector('.p');
+const operators=document.querySelectorAll('.btn__orange');
+console.log(operators);
 let clear=document.getElementById('clear');
+const numb=document.querySelectorAll('.num');
+console.log(numb);
+//Variables
+
+ Display=[];
+ 
+ //Number button
 
 
-const button0=document.getElementsByClassName('0')[0];
-const button1=document.getElementsByClassName('1')[0];
-const button2=document.getElementsByClassName('2')[0];
-const button3=document.getElementsByClassName('3')[0];
-const button4=document.getElementsByClassName('4')[0];
-const button5=document.getElementsByClassName('5')[0];
-const button6=document.getElementsByClassName('6')[0];
-const button7=document.getElementsByClassName('7')[0];
-const button8=document.getElementsByClassName('8')[0];
-const button9=document.getElementsByClassName('9')[0];
+ for (let i = 0; i < numb.length; i++) {
+  numb[i].addEventListener("click", function() {
 
-clear.addEventListener('click',(e)=>{
+       Display.push(numb[i].innerHTML)
+     resultScreen.innerHTML=Display.join('')
+     console.log(resultScreen.innerHTML);
+  });
+}
+//Ops button
 
-const newResult=[" "];
-resultScreen.innerHTML=newResult;
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", function() {
 
-})
-
-
-
-
-operators.forEach(operator=>{
-  operator.addEventListener('click',(e)=>{
-
-    resultScreen.innerHTML+=e.target.innerHTML
-   
-  })
-})
-
-
-button0.addEventListener('click',(e)=>{
-
-button0.classList.add('new');
-
-let newResult =[button0.innerHTML];
-const resultScreen=document.querySelector('p');
-resultScreen.innerHTML+=newResult;
-
-
-})
-
-
-button1.addEventListener('click',(e)=>{
-button1.classList.add('new');
-
-let newResult =[button1.innerHTML];
-const resultScreen=document.querySelector('p');
-resultScreen.innerHTML+=newResult;
-
-
-})
-button2.addEventListener('click',(e)=>{
-button2.classList.add('new');
-
-let newResult =[button2.innerHTML];
-const resultScreen=document.querySelector('p');
-resultScreen.innerHTML+=newResult;
-})
-
-button3.addEventListener('click',(e)=>{
-  button3.classList.add('new');
-  
-  let newResult =[button3.innerHTML];
-  const resultScreen=document.querySelector('p');
-  resultScreen.innerHTML+=newResult;
-  })
-  button4.addEventListener('click',(e)=>{
-    button4.classList.add('new');
-    
-    let newResult =[button4.innerHTML];
-    const resultScreen=document.querySelector('p');
-    resultScreen.innerHTML+=newResult;
-    })
-    button5.addEventListener('click',(e)=>{
-      button5.classList.add('new');
-      
-      let newResult =[button5.innerHTML];
-      const resultScreen=document.querySelector('p');
-      resultScreen.innerHTML+=newResult;
-      })
-      button6.addEventListener('click',(e)=>{
-        button6.classList.add('new');
-        
-        let newResult =[button6.innerHTML];
-        const resultScreen=document.querySelector('p');
-        resultScreen.innerHTML+=newResult;
-        })
-        button7.addEventListener('click',(e)=>{
-          button7.classList.add('new');
-          
-          let newResult =[button7.innerHTML];
-          const resultScreen=document.querySelector('p');
-          resultScreen.innerHTML+=newResult;
-          })
-          button8.addEventListener('click',(e)=>{
-            button8.classList.add('new');
-            
-            let newResult =[button8.innerHTML];
-            const resultScreen=document.querySelector('p');
-            resultScreen.innerHTML+=newResult;
-            })
-            button9.addEventListener('click',(e)=>{
-              button9.classList.add('new');
-              
-              let newResult =[button9.innerHTML];
-              const resultScreen=document.querySelector('p');
-              resultScreen.innerHTML+=newResult;
-              })
-              
-              
-//buttonFunction.addEventListener('click',(e)=>{
-
-  
-  //let newResult =[buttonFunction.innerHTML];
-  //const resultScreen=document.querySelector('p');
-  //resultScreen.innerHTML=newResult;
-  
-  
-  //})
-
-   //create new variable as array to store resultScreen data           
-
-//when (=) is pressed split array (commas) and convert to mathematical equation 
-// For example [1,+,5,-,4] -> 1+5-4 -> assign to variable which when display result of equation on the screen
-
-   //resultScreen=addResult.split(',') or similar ->extract array to make it plain which is basically math function
-   //add the answer of math function to result screen
+  Display.push(operators[i].innerHTML)
+   let news = Display.join('')
+     resultScreen.innerHTML=news
+     console.log(resultScreen.innerHTML);
+  });
+}
 
 
 
-//split array -> make it as mathematical function -> display result on = being pressed
 
-
-
-//additional-> c to clear the screen (seperate function)
-  //if(resultSceen != 0){resultScreen=0} or similar
-
-// +/- to make positive and negative numbers
-  // convert either result or button to +/- i.e. if result is 5 it can become -5 after pressing +/- button
-  // [5,+,9]->[14]-> press +/- becomes [-,14] or [5] -> +/- -> [-,5] -> +4 ->[-,5,+,4] -> [-1]
-
-// % makes number n/100 
-  //takes result or current number in array and divides from 100
