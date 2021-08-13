300f74e8465c4c53aae70103a8825edbec6254d2
david <deividasmataitis@gmail.com>
2021-08-13 22:57:21 +0100

    styling done

--- app.dev.js
+++ app.dev.js
@@ -1,37 +1,118 @@
 "use strict";
 
-//Consts
-var resultScreen = document.querySelector('.p');
-var operators = document.querySelectorAll('.btn__orange');
-console.log(operators);
-var clear = document.getElementById('clear');
-var numb = document.querySelectorAll('.num');
-console.log(numb); //Variables
+//use strict to prevent using what is not variables
+(function () {
+  "use strict"; // Shortcut to get elements by calling the ID pass which if passed returns single item
 
-Display = []; //Number button
-
-var _loop = function _loop(i) {
-  numb[i].addEventListener("click", function () {
-    Display.push(numb[i].innerHTML);
-    resultScreen.innerHTML = Display.join('');
-    console.log(resultScreen.innerHTML);
-  });
-};
-
-for (var i = 0; i < numb.length; i++) {
-  _loop(i);
-} //Ops button
+  var el = function el(item) {
+    if (item.charAt(0) === "#") {
+      return document.querySelector(item);
+    } else {
+      return document.querySelectorAll(item);
+    }
+  }; // Variables
 
 
-var _loop2 = function _loop2(_i) {
-  operators[_i].addEventListener("click", function () {
-    Display.push(operators[_i].innerHTML);
-    var news = Display.join('');
-    resultScreen.innerHTML = news;
-    console.log(resultScreen.innerHTML);
-  });
-};
+  var viewer = el("#viewer");
+  var equals = el("#equals");
+  var nums = el(".num");
+  var ops = el(".ops");
+  var theNum = "";
+  var oldNum = "";
+  var resultNum;
+  var operator; // call the number (using attribute) and display it
 
-for (var _i = 0; _i < operators.length; _i++) {
-  _loop2(_i);
-}
+  var setNum = function setNum() {
+    if (resultNum) {
+      theNum = this.getAttribute("data-num");
+      resultNum = "";
+    } else {
+      theNum += this.getAttribute("data-num");
+    }
+
+    viewer.innerHTML = theNum;
+  }; // call operator and pass current number to old number
+
+
+  var moveNum = function moveNum() {
+    oldNum = theNum;
+    theNum = "";
+    operator = this.getAttribute("data-ops");
+    equals.setAttribute("data-result", "");
+  }; // Equal operator and result calculation
+
+
+  var displayNum = function displayNum() {
+    oldNum = parseFloat(oldNum);
+    theNum = parseFloat(theNum); // Switch function to perform calculation
+
+    switch (operator) {
+      case "plus":
+        resultNum = oldNum + theNum;
+        break;
+
+      case "minus":
+        resultNum = oldNum - theNum;
+        break;
+
+      case "times":
+        resultNum = oldNum * theNum;
+        break;
+
+      case "divided by":
+        resultNum = oldNum / theNum;
+        break;
+
+      case "percentage":
+        resultNum = oldNum / 100;
+        break;
+      // If equal is pressed without an operator, keep the number
+
+      default:
+        resultNum = theNum;
+    } // Error(e.g. double press on operators or dividing from 0) and infinity handling
+
+
+    if (!isFinite(resultNum)) {
+      if (isNaN(resultNum)) {
+        resultNum = "Infinity!";
+      } else {
+        resultNum = "Stop dividing from 0, this does not work!";
+      }
+    } // Display result and reset old number
+
+
+    viewer.innerHTML = resultNum;
+    equals.setAttribute("data-result", resultNum);
+  }; // Clear button
+
+
+  var clearAll = function clearAll() {
+    oldNum = "";
+    theNum = "";
+    viewer.innerHTML = "0";
+    equals.setAttribute("data-result", resultNum);
+  }; // Click number
+
+
+  for (var i = 0, l = nums.length; i < l; i++) {
+    nums[i].onclick = setNum;
+  } // Click operator
+
+
+  for (var _i = 0, _l = ops.length; _i < _l; _i++) {
+    ops[_i].onclick = moveNum;
+  } // Equal sign operator
+
+
+  equals.onclick = displayNum; // Limit numbers
+
+  var max_chars = 5;
+
+  if (displayNum.innerHMTL.length > max_chars) {
+    displayNum.innerHTML = displayNum.innerHTML.substr(0, max_chars);
+  } // Clear button
+
+
+  el("#clear").onclick = clearAll;
+})();
--- app.js
+++ app.js
@@ -1,40 +1,125 @@
+//use strict to prevent using what is not variables
+(function() {
 
+  "use strict";
 
-//Consts
+  // Shortcut to get elements by calling the ID pass which if passed returns single item
+  const el = function(item) {
+    if (item.charAt(0) === "#") {
+      return document.querySelector(item); 
+    }else{
+      return document.querySelectorAll(item);
+    }
+  };
 
-const resultScreen=document.querySelector('.p');
-const operators=document.querySelectorAll('.btn__orange');
-console.log(operators);
-let clear=document.getElementById('clear');
-const numb=document.querySelectorAll('.num');
-console.log(numb);
-//Variables
+  // Variables
+   let viewer = el("#viewer");
+   let equals = el("#equals");
+   let nums = el(".num");
+   let ops = el(".ops"); 
+   let theNum = ""; 
+   let oldNum = "";
+   let resultNum; 
+   let operator; 
 
- Display=[];
+  // call the number (using attribute) and display it
+  const setNum = function() {
+    if (resultNum) {
+      theNum = this.getAttribute("data-num");
+      resultNum = "";
+    } else { 
+      theNum += this.getAttribute("data-num");
+    }
+    viewer.innerHTML = theNum; 
+  };
+
+  // call operator and pass current number to old number
+  const moveNum = function() {
+    oldNum = theNum;
+    theNum = "";
+    operator = this.getAttribute("data-ops");
+    equals.setAttribute("data-result", ""); 
+  };
+
+  // Equal operator and result calculation
+  const displayNum = function() {
+
+    oldNum = parseFloat(oldNum);
+    theNum = parseFloat(theNum);
+
+    // Switch function to perform calculation
+    switch (operator) {
+      case "plus":
+        resultNum = oldNum + theNum;
+        break;
+
+      case "minus":
+        resultNum = oldNum - theNum;
+        break;
+
+      case "times":
+        resultNum = oldNum * theNum;
+        break;
+
+      case "divided by":
+        resultNum = oldNum / theNum;
+        break;
+
+      case "percentage":
+        resultNum = oldNum / 100;
+        break;
+
+        // If equal is pressed without an operator, keep the number
+      default:
+        resultNum = theNum;
+    }
+
+    // Error(e.g. double press on operators or dividing from 0) and infinity handling
+    if (!isFinite(resultNum)) {
+      if (isNaN(resultNum)) { 
+        resultNum = "Infinity!";
+      } else {
+        resultNum = "Stop dividing from 0, this does not work!";
+      }
+    }
+
+    // Display result and reset old number
+    viewer.innerHTML = resultNum;
+    equals.setAttribute("data-result", resultNum);
+
+  };
+
+  // Clear button
+  const clearAll = function() {
+    oldNum = "";
+    theNum = "";
+    viewer.innerHTML = "0";
+    equals.setAttribute("data-result", resultNum);
+  };
+
  
- //Number button
 
+  // Click number
+  for (let i = 0, l = nums.length; i < l; i++) {
+    nums[i].onclick = setNum;
+  }
 
- for (let i = 0; i < numb.length; i++) {
-  numb[i].addEventListener("click", function() {
+  // Click operator
+  for (let i = 0, l = ops.length; i < l; i++) {
+    ops[i].onclick = moveNum;
+  }
 
-       Display.push(numb[i].innerHTML)
-     resultScreen.innerHTML=Display.join('')
-     console.log(resultScreen.innerHTML);
-  });
-}
-//Ops button
+  // Equal sign operator
+  equals.onclick = displayNum;
 
-for (let i = 0; i < operators.length; i++) {
-  operators[i].addEventListener("click", function() {
+  // Limit numbers
+ let max_chars=5
 
-  Display.push(operators[i].innerHTML)
-   let news = Display.join('')
-     resultScreen.innerHTML=news
-     console.log(resultScreen.innerHTML);
-  });
-}
+  if(displayNum.innerHMTL.length > max_chars) {
+      displayNum.innerHTML = displayNum.innerHTML.substr(0, max_chars);
+  }
 
+  // Clear button
+  el("#clear").onclick = clearAll;
 
-
-
+}());
